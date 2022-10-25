7f8573386124443bfd429ace135e1be07c67fba4
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-10-25 16:20:55 -0600

    good functionality all merged so far

--- src/components/CandyBucket.js
+++ src/components/CandyBucket.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import handleRandomCandy from "./handleRandomCandy";
-
 function CandyBucket(){
-
-
+    const [candies, setCandies] = useState([])
+    function handleClick(){
+        setCandies(candies.concat(handleRandomCandy()))
+    }
+console.log(candies)
     return (
-        <div className="bucket-container" onClick={handleRandomCandy}>
+        <div className="bucket-container" onClick={handleClick}>
             <img className="bucket" src="https://i.imgur.com/3ZwfAoM.png"></img>
+            {candies.map(candy=><img className="candy" key={candy.id} src={candy.image}/>)}
         </div>
     )
 }
-
 export default CandyBucket
