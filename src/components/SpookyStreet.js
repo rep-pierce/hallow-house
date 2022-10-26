456f65ce28fb00626342dabd8c0266f4476e4d55
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-10-26 08:10:39 -0700

    little ghost

--- src/components/SpookyStreet.js
+++ src/components/SpookyStreet.js
@@ -3,10 +3,13 @@ import HouseCard from "./HouseCard"
 import EnergyBar from "./EnergyBar"
 import CandyBucket from "./CandyBucket"
 import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 function SpookyStreet({ houses, houseIndex, setHouseIndex, ghostLoc, setGhostLoc }) {
     let start = houseIndex
     let end = houseIndex + 4
+
+    const navigate = useNavigate()
 
     function renderHouses() {
         const fourHouses = houses.slice(start, end)
@@ -23,6 +26,9 @@ function SpookyStreet({ houses, houseIndex, setHouseIndex, ghostLoc, setGhostLoc
             else if (e.key==='ArrowLeft'){
                 setGhostLoc(ghostLoc-5)
             }
+            else if (e.key==='ArrowUp'){
+                navigate('/porch')
+            }
         }
 
         document.addEventListener('keydown', handleKeyPress)
@@ -32,21 +38,31 @@ function SpookyStreet({ houses, houseIndex, setHouseIndex, ghostLoc, setGhostLoc
         }
     })
 
+    if (ghostLoc>680){
+        setGhostLoc(-590)
+        setHouseIndex(houseIndex+4)
+    }
+
+    if (ghostLoc<-595){
+        setGhostLoc(675)
+        setHouseIndex(houseIndex-4)
+    }
+
     return (
         <div>
             <div className="belt">
                 {renderHouses()}
-                <button onClick={() => setHouseIndex(houseIndex + 1)}>keep walkin</button>
+                <button className="more-button" onClick={() => setHouseIndex(houseIndex + 1)}>keep walkin</button>
             </div>
             <div className="road-div">
                 <img
                     src="https://cliparting.com/wp-content/uploads/2016/10/Road-bitumen-clipart-by-megapixl.jpg"
-                    style={{ width: "100%", height: "150px" }}
+                    style={{ width: "100%", height: "150px", position: "absolute", left: "0%"}}
                 />
                 <img
-                    src="https://i.imgur.com/vIXvECK.png"
+                    src="https://i.imgur.com/2TOqj6t.png"
                     className="avatar"
-                    style={{ left: `${ghostLoc}px`}}
+                    style={{left: `${ghostLoc}px`}}
                 />
             </div>
             <div className="grass">.</div>
